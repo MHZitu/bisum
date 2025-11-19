24b75698a9c2094dc81388bd37fe8ff7adc4f098
MHZitu <mh6599068@gmail.com>
2025-11-20 02:14:38 +0600

    my name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
- var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
       pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
       },
     });
+
+var swiper = new Swiper(".testimonialSwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: ".customPagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
